e5556c630343c9b52729d21168bce99489cb2a87
Attila Gazso <agazso@gmail.com>
2018-04-09 12:18:51 +0200

    Changed componentWillUnmount to componentDidUnmount in PostScreen

--- src/components/PostScreen.tsx
+++ src/components/PostScreen.tsx
@@ -130,7 +130,7 @@ export class PostScreen extends React.Component<any, any> {
 
     }
 
-    public componentWillUnmount() {
+    public componentDidlUnmount() {
         this.unregisterListeners();
     }
 
