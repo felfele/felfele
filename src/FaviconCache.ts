fe49ecb931e174d091cd50361b45112a291a3279
Attila Gazso <agazso@gmail.com>
2018-07-04 12:05:56 +0200

    Fixed favicon extraction from html and favicon caching

--- src/FaviconCache.ts
+++ src/FaviconCache.ts
@@ -1,3 +1,5 @@
+import { HtmlUtils } from './HtmlUtils';
+
 class _FaviconCache {
     private favicons: Map<string, string> = new Map();
 
@@ -8,6 +10,7 @@ class _FaviconCache {
         const baseUrl = this.getBaseUrl(url);
         try {
             const favicon = await this.downloadIndexAndParseFavicon(baseUrl);
+            console.log('getFavicon: ', favicon);
             this.favicons.set(url, favicon);
             return favicon;
         } catch (e) {
@@ -24,12 +27,44 @@ class _FaviconCache {
         return baseUrl;
     }
 
-    private async downloadIndexAndParseFavicon(url) {
-        const defaults = {
-            'https://444.hu/': 'https://444.hu/assets/blog/static/444-touch-60x60-9e787b1cc6bb60204e7419276bc82d59.png',
-        };
+    private async fetchHtml(url): Promise<string> {
+        const response = await fetch(url);
+        if (response.status !== 200) {
+            throw new Error('failed to download html');
+        }
 
-        return defaults.hasOwnProperty(url) ? defaults[url] : url + 'favicon.ico';
+        const html = await response.text();
+        return html;
+
+    }
+
+    private matchRelAttributes(node: Node, values: string[]): string | null {
+        for (const value of values) {
+            if (HtmlUtils.matchAttributes(node, [{name: 'rel', value: value}])) {
+                const favicon = HtmlUtils.getAttribute(node, 'href') || '';
+                if (favicon !== '') {
+                    return favicon;
+                }
+            }
+        }
+        return null;
+    }
+
+    private async downloadIndexAndParseFavicon(url) {
+        const html = await this.fetchHtml(url);
+        const document = HtmlUtils.parse(html);
+        const links = HtmlUtils.findPath(document, ['html', 'head', 'link']);
+        for (const link of links) {
+            const icon = this.matchRelAttributes(link, ['shortcut icon', 'icon', 'apple-touch-icon']);
+            if (icon != null) {
+                if (!icon.startsWith('http') && !icon.startsWith('//')) {
+                    return url + icon;
+                }
+                return icon;
+            }
+        }
+
+        return url + 'favicon.ico';
     }
 }
 
