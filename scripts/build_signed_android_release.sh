544e8e85a2644a2275b6fd02336b2fff957375ea
Attila Gazso <agazso@gmail.com>
2018-11-16 00:32:45 +0530

    Added react-native-get-real-path for android image path resolution

--- scripts/build_signed_android_release.sh
+++ scripts/build_signed_android_release.sh
@@ -7,5 +7,5 @@ react-native bundle --platform android --dev false --entry-file index.android.js
 cd android && ./gradlew assembleRelease && cd ..
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/.android/debug.keystore -storepass android -keypass android android/app/build/outputs/apk/release/app-release-unsigned.apk androiddebugkey
 
-# After this you can upload the apk to the device with:
-#  adb push app/build/outputs/apk/release/app-release-unsigned.apk /storage/self/primary/Download
+echo "Now you can upload the apk to the device with the following command:"
+echo " adb -d push android/app/build/outputs/apk/release/app-release-unsigned.apk /storage/self/primary/Download"
