4a7fac22ee21e52041d66194cde97cc6cc9437e7
Attila Gazso <agazso@gmail.com>
2018-11-18 20:21:49 +0530

    Removed unused code

--- src/PostFeed.ts
+++ src/PostFeed.ts
@@ -53,26 +53,6 @@ export const updatePostFeed = async (swarmFeedApi: Swarm.FeedApi, postFeed: Post
     }
 };
 
-const updateHashFromService = async (locationHash: string, contentHash: string): Promise<string> => {
-    const timestamp = Date.now();
-    const body = JSON.stringify({contentHash, locationHash, timestamp});
-    const rawResponse = await fetch(HASH_SERVICE_URL, {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body,
-    });
-    const response = await rawResponse.json();
-    Debug.log('updateHash: response: ', response, rawResponse.status, body);
-    return response.id;
-};
-
-const downloadHashFromService = async (locationHash: string): Promise<string> => {
-    const rawResponse = await fetch(HASH_SERVICE_URL + '?locationHash=' + locationHash + '&_sort=timestamp&_order=desc&_limit=1');
-    const response = await rawResponse.json();
-    Debug.log('downloadHash: response: ', response, response.contentHash, rawResponse.status);
-    return response[0].contentHash;
-};
-
 export const downloadPostFeed = async (swarmFeedApi: Swarm.FeedApi, url: string): Promise<PostFeed> => {
     const contentHash = await swarmFeedApi.downloadFeed(url);
     Debug.log('downloadPostFeed: contentHash: ', contentHash);
