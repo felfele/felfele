609227619c09d508ce8913a3f49ca476bcafd41c
Attila Gazso <agazso@gmail.com>
2018-11-22 21:36:23 +0530

    Fixed iOS build by relinking with newer version of react-native-svg

--- android/app/src/main/java/com/postmodern/MainApplication.java
+++ android/app/src/main/java/com/postmodern/MainApplication.java
@@ -3,8 +3,8 @@ package com.postmodern;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import net.rhogan.rnsecurerandom.RNSecureRandomPackage;
 import com.horcrux.svg.SvgPackage;
+import net.rhogan.rnsecurerandom.RNSecureRandomPackage;
 import com.rnfs.RNFSPackage;
 import org.reactnative.camera.RNCameraPackage;
 import com.imagepicker.ImagePickerPackage;
@@ -30,8 +30,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNSecureRandomPackage(),
             new SvgPackage(),
+            new RNSecureRandomPackage(),
             new RNFSPackage(),
             new RNCameraPackage(),
             new ImagePickerPackage(),
