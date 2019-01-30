ff73d4aa7fcc31b180972c0f0b1d0393babdfbe3
Attila Gazso <agazso@gmail.com>
2019-01-30 13:07:32 +0700

    Release script improvements

--- scripts/release_new_version.sh
+++ scripts/release_new_version.sh
@@ -29,14 +29,15 @@ ask "Upload the build to the App Store"
 ask "In AppstoreConnect provide the crypto information to enable the new build"
 
 echo "Building the android release version signed with debug key..."
-./scripts/build_signed_android_release.sh
+ask "TODO: ./scripts/build_signed_android_release.sh"
 
 ask "Upload the android version to https://app.felfele.com"
 
 echo "Tagging the git release with v$version"
-./scripts/git_tag_release.sh "$version"
+ask "TODO: ./scripts/git_tag_release.sh $version"
 
 echo "Updating the git release branch with to the master..."
 ask "TODO: ./scripts/git_update_release_branch.sh"
 
 ask "Announce the release on Slack in the #releases channel"
+echo "Release of version $version was successful!"
