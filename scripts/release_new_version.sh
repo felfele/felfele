f78deb2547eafee8e3d90804908566a9737c7109
Attila Gazso <agazso@gmail.com>
2019-01-30 13:00:34 +0700

    Added the missing parts of the release checklist to the release script as confirmation from user

--- scripts/release_new_version.sh
+++ scripts/release_new_version.sh
@@ -1,9 +1,42 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
+function ask {
+    echo
+    read -p "$1, press y when done: " -n1 input
+    echo
+    if [ "$input" != "y" ]; then
+        exit 1
+    fi
+}
+
+echo "Running checks locally..."
 npm run check
 
+echo "Bumping the build number..."
 ./scripts/bump_build_number.sh
-./scripts/git_tag_release.sh "$(./scripts/cli.sh version)"
+version="$(./scripts/cli.sh version)"
 
-echo "TODO: ./scripts/git_update_release_branch.sh"
-echo "TODO: ./scripts/build_signed_android_release.sh"
+echo "Commit and push changes to the repo"
+commit_message="Bumped version to $version"
+ask "TODO: git commit -am \"$commit_message\" && git push"
+
+ask "Check if the CI is green"
+
+ask "Build the iOS version with XCode for archive"
+
+ask "Upload the build to the App Store"
+
+ask "In AppstoreConnect provide the crypto information to enable the new build"
+
+echo "Building the android release version signed with debug key..."
+./scripts/build_signed_android_release.sh
+
+ask "Upload the android version to https://app.felfele.com"
+
+echo "Tagging the git release with v$version"
+./scripts/git_tag_release.sh "$version"
+
+echo "Updating the git release branch with to the master..."
+ask "TODO: ./scripts/git_update_release_branch.sh"
+
+ask "Announce the release on Slack in the #releases channel"
