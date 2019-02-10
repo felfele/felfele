6ff0d7ef5d506fe458399fe5f5cd62d79f2aa5c4
Mark Vujevits <mark.vujevits@prezi.com>
2019-02-10 14:45:01 +0100

    Revert "try to fix markdown hashtag bug"
    This reverts commit e85bf2bfed804b01be949204d0389e3fb23e07c9.

--- src/markdown.ts
+++ src/markdown.ts
@@ -1,7 +1,7 @@
 export const markdownEscape = (markdown: string): string => {
-    return markdown.replace(/^#/g, '\\#');
+    return markdown.replace(/([\\\`*_{}\(\)\[\]#\+\-\.!])/g, '\\$1');
 };
 
 export const markdownUnescape = (markdown: string): string => {
-    return markdown.replace(/^\\#/g, '#');
+    return markdown.replace(/(\\([\\\`*_{}\(\)\[\]#\+\-\.!]))/g, '$2');
 };
--- test/markdownTest.ts
+++ test/markdownTest.ts
@@ -8,45 +8,10 @@ test('Basic markdown escape', () => {
     expect(result).toEqual(escapedText);
 });
 
-test('double markdown escape', () => {
-    const markdownText = '#hashtag';
-    const escapedText = '\\' + markdownText;
-    const preResult = markdownEscape(markdownText);
-    const result = markdownEscape(preResult);
-
-    expect(result).toEqual(escapedText);
-});
-
-// why equal?
-test('Basic markdown unescape', () => {
-    const markdownText = '\#hashtag';
-    const unescapedText = '#hashtag';
-    const result = markdownUnescape(markdownText);
-
-    expect(result).not.toEqual(unescapedText);
-});
-
 test('Basic markdown unescape', () => {
     const markdownText = '\\#hashtag';
     const unescapedText = '#hashtag';
     const result = markdownUnescape(markdownText);
 
     expect(result).toEqual(unescapedText);
-});
-
-// why equal?
-test('Does not unescape other starting text', () => {
-    const markdownText = '\\\#hashtag';
-    const unescapedText = '#hashtag';
-    const result = markdownUnescape(markdownText);
-
-    expect(result).not.toEqual(unescapedText);
-});
-
-test('Does not unescape other starting text', () => {
-    const markdownText = '\\\\#hashtag';
-    const unescapedText = '#hashtag';
-    const result = markdownUnescape(markdownText);
-
-    expect(result).not.toEqual(unescapedText);
 });
