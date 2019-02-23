695f3a5f2738b801142ea9e3db05582c59f72acd
Mark Vujevits <mark.vujevits@prezi.com>
2019-02-23 17:09:01 +0100

    fixes #176

--- src/containers/FeedContainer.ts
+++ src/containers/FeedContainer.ts
@@ -3,14 +3,18 @@ import { AppState } from '../reducers';
 import { StateProps, DispatchProps, FeedView } from '../components/FeedView';
 import { AsyncActions, Actions } from '../actions/Actions';
 import { Feed } from '../models/Feed';
-import { getFeedPosts } from '../selectors/selectors';
+import { getFeedPosts, getYourPosts } from '../selectors/selectors';
 
 export const mapStateToProps = (state: AppState, ownProps: { navigation: any }): StateProps => {
     const feedUrl = ownProps.navigation.state.params.feedUrl;
     const selectedFeeds = state.feeds.filter(feed => feed != null && feed.feedUrl === feedUrl);
+    // TODO: we should have here state.ownFeeds.find(feed => feed.url === feedUrl) != null,
+    // but author.uri is empty, and the feedUrl from navparams is from post.author.uri (not real feedUrl)
+    // For the same reason feeds/selectedFeeds is empty (errounously)
+    const isOwnFeed = state.author.uri === feedUrl;
     // Note: this is a moderately useful selector (recalculated if another feedUrl is opened (cache size == 1))
     // see https://github.com/reduxjs/reselect/blob/master/README.md#accessing-react-props-in-selectors
-    const posts = getFeedPosts(state, feedUrl);
+    const posts = isOwnFeed ? getYourPosts(state) : getFeedPosts(state, feedUrl);
     return {
         onBack: () => ownProps.navigation.goBack(null),
         navigation: ownProps.navigation,
@@ -19,10 +23,7 @@ export const mapStateToProps = (state: AppState, ownProps: { navigation: any }):
         posts: posts,
         // HACK
         feeds: selectedFeeds.length === 0 ? [state.ownFeeds[0]] : selectedFeeds,
-        // TODO: we should have here state.ownFeeds.find(feed => feed.url === feedUrl) != null,
-        // but author.uri is empty, and the feedUrl from navparams is from post.author.uri (not real feedUrl)
-        // For the same reason feeds/selectedFeeds is empty (errounously)
-        isOwnFeed: state.author.uri === feedUrl,
+        isOwnFeed: isOwnFeed,
     };
 };
 
