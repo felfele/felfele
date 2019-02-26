55d822796309d340545289be81bf563a3e97b2b6
Mark Vujevits <mark.vujevits@prezi.com>
2019-02-26 11:36:13 +0100

    loading screen added, some resolutions are missing for iOS

--- android/app/src/main/java/com/postmodern/MainActivity.java
+++ android/app/src/main/java/com/postmodern/MainActivity.java
@@ -1,6 +1,8 @@
 package com.postmodern;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +13,11 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "postmodern";
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);
+        super.onCreate(savedInstanceState);
     }
 }
