1171784770dd30e4e135fee85dcbc98a98be419e
Attila Gazso <agazso@gmail.com>
2019-03-01 20:22:53 +0530

    Revert "fix not being able to open own posts until shared"
    This reverts commit ff53bbc34d33376798c6d8155d7ad97c25386ab7.

--- src/containers/FeedContainer.ts
+++ src/containers/FeedContainer.ts
@@ -22,9 +22,7 @@ export const mapStateToProps = (state: AppState, ownProps: { navigation: any }):
         feedName: ownProps.navigation.state.params.name,
         posts: posts,
         // HACK
-        feeds: selectedFeeds.length === 0
-            ? state.ownFeeds[0] ? state.ownFeeds : []
-            : selectedFeeds,
+        feeds: selectedFeeds.length === 0 ? [state.ownFeeds[0]] : selectedFeeds,
         isOwnFeed: isOwnFeed,
         gatewayAddress: state.settings.swarmGatewayAddress,
     };
