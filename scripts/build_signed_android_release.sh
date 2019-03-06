2acba712c7fe35fc8ff22bcd804a636be06e01d6
Attila Gazso <agazso@gmail.com>
2019-03-06 13:04:21 +0530

    Fixed android build bundle phase

--- scripts/build_signed_android_release.sh
+++ scripts/build_signed_android_release.sh
@@ -2,7 +2,7 @@
 #
 # Build a release version and sign with debug key
 
-react-native bundle --platform android --dev false --entry-file index.android.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res
+npm run bundle:android
 
 # above step is supposed to produce some unneeded assets - https://github.com/facebook/react-native/issues/19239#issuecomment-414564404
 rm -r android/app/src/main/res/drawable-*
