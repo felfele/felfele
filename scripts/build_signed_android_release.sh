52e7d678539a069bf5079033aed0ca68d52cf641
Attila Gazso <agazso@gmail.com>
2019-03-06 22:52:46 +0530

    Added debug keystore to the repo

--- scripts/build_signed_android_release.sh
+++ scripts/build_signed_android_release.sh
@@ -7,7 +7,7 @@ npm run bundle:android
 # above step is supposed to produce some unneeded assets - https://github.com/facebook/react-native/issues/19239#issuecomment-414564404
 rm -r android/app/src/main/res/drawable-*
 cd android && ./gradlew --quiet --console plain assembleRelease && cd ..
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/.android/debug.keystore -storepass android -keypass android android/app/build/outputs/apk/release/app-release-unsigned.apk androiddebugkey
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore android/debug.keystore -storepass android -keypass android android/app/build/outputs/apk/release/app-release-unsigned.apk androiddebugkey
 
 echo
 echo "Now you can upload the apk to the device with the following command:"
