2c167667c3a0f93669b45d43644d3ca1887032f4
Attila Gazso <agazso@gmail.com>
2019-03-06 22:15:04 +0530

    Turned off gradle rich output

--- scripts/build_signed_android_release.sh
+++ scripts/build_signed_android_release.sh
@@ -6,7 +6,7 @@ npm run bundle:android
 
 # above step is supposed to produce some unneeded assets - https://github.com/facebook/react-native/issues/19239#issuecomment-414564404
 rm -r android/app/src/main/res/drawable-*
-cd android && ./gradlew assembleRelease && cd ..
+cd android && ./gradlew --quiet --console plain assembleRelease && cd ..
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/.android/debug.keystore -storepass android -keypass android android/app/build/outputs/apk/release/app-release-unsigned.apk androiddebugkey
 
 echo
