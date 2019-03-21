d02c8c4a0297188eacba0b45062064ef2683bd6a
Attila Gazso <agazso@gmail.com>
2019-03-21 08:10:42 +0100

    First step of FeedView refactor: got rid of feed array in properties, only the selected feed is passed

--- src/components/FeedView.tsx
+++ src/components/FeedView.tsx
@@ -18,58 +18,61 @@ export interface DispatchProps {
     onRemoveFeed: (feed: Feed) => void;
 }
 
+export interface ViewFeed extends Feed {
+    isOwnFeed: boolean;
+    isLocalFeed: boolean;
+}
+
 export interface StateProps {
     navigation: any;
     onBack: () => void;
-    feedUrl: string;
-    feedName: string;
+    feed: ViewFeed;
     posts: Post[];
-    feeds: Feed[];
-    isOwnFeed: boolean;
     gatewayAddress: string;
 }
 
 type Props = StateProps & DispatchProps;
 
 export const FeedView = (props: Props) => {
-    const isFollowedFeed = props.feeds.find(feed => feed.feedUrl === props.feedUrl && feed.followed === true) != null;
     const modelHelper = new ReactNativeModelHelper(props.gatewayAddress);
-    const isLocalFeed = props.isOwnFeed || props.feeds.length === 0;
     const icon = (name: string, color: string) => <Icon name={name} size={20} color={color} />;
     const button = (iconName: string, color: string, onPress: () => void) => ({
         label: icon(iconName, color),
         onPress,
     });
-    const toggleFavorite = () => props.onToggleFavorite(props.feedUrl);
+    const toggleFavorite = () => props.onToggleFavorite(props.feed.feedUrl);
     const navigateToFeedSettings = () => props.navigation.navigate(
         'FeedSettings',
-        { feed: props.feeds[0] },
+        { feed: props.feed },
     );
-    const onLinkPressed = async () => onFollowPressed(props.feedUrl,
-        props.feeds,
+    const onLinkPressed = async () => onFollowPressed(props.feed,
         props.onUnfollowFeed,
         props.onFollowFeed
     );
-    const rightButton1 = props.isOwnFeed
-        ? props.feedName.length > 0
+    const rightButton1 = props.feed.isOwnFeed
+        ? props.feed.name.length > 0
             ? button('settings-box', Colors.DARK_GRAY, navigateToFeedSettings)
             : undefined
-        : isFollowedFeed
-            ? isFavorite(props.feeds, props.feedUrl)
+        : props.feed.followed === true
+            ? props.feed.favorite === true
                 ? button('star', Colors.BRAND_PURPLE, toggleFavorite)
                 : button('star', Colors.DARK_GRAY, toggleFavorite)
-            : props.feedName === FELFELE_ASSISTANT_NAME
+            : props.feed.name === FELFELE_ASSISTANT_NAME
                 ? undefined
                 : button('delete', Colors.DARK_GRAY, () => removeFeedAndGoBack(props))
     ;
-    const rightButton2 = isLocalFeed
+    const rightButton2 = props.feed.isLocalFeed
         ? undefined
-        : isFollowedFeed
+        : props.feed.followed === true
             ? button('link-variant-off', Colors.DARK_GRAY, onLinkPressed)
             : button('link-variant', Colors.DARK_GRAY, onLinkPressed)
     ;
+    const refreshableFeedProps = {
+        ...props,
+        feeds: [props.feed],
+    };
     return (
-        <RefreshableFeed modelHelper={modelHelper} {...props}>
+        <RefreshableFeed modelHelper={modelHelper} {...refreshableFeedProps}>
             {{
                 navigationHeader: <NavigationHeader
                     navigation={props.navigation}
@@ -79,24 +82,18 @@ export const FeedView = (props: Props) => {
                     }}
                     rightButton1={rightButton1}
                     rightButton2={rightButton2}
-                    title={props.feedName}
+                    title={props.feed.name}
                 />,
             }}
         </RefreshableFeed>
     );
 };
 
-const isFavorite = (feeds: Feed[], uri: string): boolean => {
-    const feed = feeds.find(value => value.feedUrl === uri);
-    return feed != null && !!feed.favorite;
-};
-
-const onFollowPressed = async (uri: string, feeds: Feed[], onUnfollowFeed: (feed: Feed) => void, onFollowFeed: (feed: Feed) => void) => {
-    const followedFeed = feeds.find(feed => feed.feedUrl === uri && feed.followed === true);
-    if (followedFeed != null) {
-        await unfollowFeed(followedFeed, onUnfollowFeed);
+const onFollowPressed = async (feed: Feed, onUnfollowFeed: (feed: Feed) => void, onFollowFeed: (feed: Feed) => void) => {
+    if (feed.followed === true) {
+        await unfollowFeed(feed, onUnfollowFeed);
     } else {
-        followFeed(uri, feeds, onFollowFeed);
+        onFollowFeed(feed);
     }
 };
 
@@ -107,18 +104,10 @@ export const unfollowFeed = async (feed: Feed, onUnfollowFeed: (feed: Feed) => v
     }
 };
 
-const followFeed = (uri: string, feeds: Feed[], onFollowFeed: (feed: Feed) => void) => {
-    const knownFeed = feeds.find(feed => feed.feedUrl === uri && feed.followed !== true);
-    if (knownFeed != null) {
-        onFollowFeed(knownFeed);
-    }
-};
-
 const removeFeedAndGoBack = async (props: Props) => {
     const confirmRemove = await AreYouSureDialog.show('Are you sure you want to delete?');
-    const feedToRemove = props.feeds.find(feed => feed.feedUrl === props.feedUrl && feed.followed !== true);
-    if (feedToRemove != null && confirmRemove) {
-        props.onRemoveFeed(feedToRemove);
+    if (confirmRemove) {
+        props.onRemoveFeed(props.feed);
         props.onBack();
     }
 };
--- src/containers/FeedContainer.ts
+++ src/containers/FeedContainer.ts
@@ -1,9 +1,24 @@
 import { connect } from 'react-redux';
 import { AppState, defaultLocalPosts, FELFELE_ASSISTANT_NAME } from '../reducers';
-import { StateProps, DispatchProps, FeedView } from '../components/FeedView';
+import { StateProps, DispatchProps, FeedView, ViewFeed } from '../components/FeedView';
 import { AsyncActions, Actions } from '../actions/Actions';
 import { Feed } from '../models/Feed';
 import { getFeedPosts, getYourPosts } from '../selectors/selectors';
+
+const emptyFeed = (name: string = '', isOwnFeed: boolean = false, isLocalFeed = false): ViewFeed => ({
+    name,
+    feedUrl: '',
+    url: '',
+    favicon: '',
+    isOwnFeed,
+    isLocalFeed,
+});
+
+const feedToViewFeed = (feed: Feed): ViewFeed => ({
+    ...feed,
+    isOwnFeed: false,
+    isLocalFeed: false,
+});
 
 export const mapStateToProps = (state: AppState, ownProps: { navigation: any }): StateProps => {
     const feedUrl = ownProps.navigation.state.params.feedUrl;
@@ -12,14 +27,21 @@ export const mapStateToProps = (state: AppState, ownProps: { navigation: any }):
     const isOwnFeed = feedName === state.author.name;
     const isAssistantFeed = feedName === FELFELE_ASSISTANT_NAME;
     const hasOwnFeed = state.ownFeeds.length > 0;
-    const ownFeed = hasOwnFeed ? state.ownFeeds[0] : undefined;
-    const selectedFeeds = isOwnFeed
-        ? hasOwnFeed
-            ? [ownFeed!]
-            : []
+    const ownFeed = hasOwnFeed
+        ? {
+            ...state.ownFeeds[0],
+            isOwnFeed: true,
+            isLocalFeed: true,
+        }
+        : emptyFeed('', isOwnFeed);
+    const otherFeed = state.feeds.find(feed => feed.feedUrl === feedUrl);
+    const selectedFeed = isOwnFeed
+        ? ownFeed
         : isAssistantFeed
-            ? []
-            : state.feeds.filter(feed => feed != null && feed.feedUrl === feedUrl)
+            ? emptyFeed(FELFELE_ASSISTANT_NAME, false, true)
+            : otherFeed != null
+                ? feedToViewFeed(otherFeed)
+                : emptyFeed()
         ;
     // Note: this is a moderately useful selector (recalculated if another feedUrl is opened (cache size == 1))
     // see https://github.com/reduxjs/reselect/blob/master/README.md#accessing-react-props-in-selectors
@@ -34,11 +56,8 @@ export const mapStateToProps = (state: AppState, ownProps: { navigation: any }):
     return {
         onBack: () => ownProps.navigation.goBack(null),
         navigation: ownProps.navigation,
-        feedUrl,
-        feedName,
         posts,
-        feeds: selectedFeeds,
-        isOwnFeed,
+        feed: selectedFeed,
         gatewayAddress: state.settings.swarmGatewayAddress,
     };
 };
