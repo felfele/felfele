ad38ec12a3e1748d9a7b34de119efb1620f47827
Attila Gazso <agazso@gmail.com>
2019-04-07 20:37:12 +0200

    Added react-native-markdown-renderer package

--- src/components/CardMarkdown.tsx
+++ src/components/CardMarkdown.tsx
@@ -2,25 +2,26 @@ import * as React from 'react';
 import { TouchableWithoutFeedback, Text, Linking, StyleSheet } from 'react-native';
 
 // @ts-ignore
-import Markdown from 'react-native-easy-markdown';
+// import Markdown from 'react-native-easy-markdown';
+import Markdown from 'react-native-markdown-renderer';
 import { ErrorBoundary } from './ErrorBoundary';
 
 export const CardMarkdown = (props: { text: string }) => (
     <ErrorBoundary>
         <Markdown
             style={styles.markdownStyle}
-            renderLink={(href: string, title: string, children: React.ReactNode) => {
-                return (
-                    <TouchableWithoutFeedback
-                        key={'linkWrapper_' + href + Date.now()}
-                        onPress={() => Linking.openURL(href).catch(() => { /* nothing */ })}
-                    >
-                        <Text key={'linkWrapper_' + href + Date.now()} style={{textDecorationLine: 'underline'}}>
-                            {children}
-                        </Text>
-                    </TouchableWithoutFeedback>
-                );
-            }}
+            // renderLink={(href: string, title: string, children: React.ReactNode) => {
+            //     return (
+            //         <TouchableWithoutFeedback
+            //             key={'linkWrapper_' + href + Date.now()}
+            //             onPress={() => Linking.openURL(href).catch(() => { /* nothing */ })}
+            //         >
+            //             <Text key={'linkWrapper_' + href + Date.now()} style={{textDecorationLine: 'underline'}}>
+            //                 {children}
+            //             </Text>
+            //         </TouchableWithoutFeedback>
+            //     );
+            // }}
         >{props.text}</Markdown>
     </ErrorBoundary>
 );
