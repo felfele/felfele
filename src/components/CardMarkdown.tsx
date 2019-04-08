6aa840627192f3cc5f1532079124eff615bf1033
Attila Gazso <agazso@gmail.com>
2019-04-08 21:15:43 +0200

    Fixed margin styling, removed commented legacy code

--- src/components/CardMarkdown.tsx
+++ src/components/CardMarkdown.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { TouchableWithoutFeedback, Text, Linking, StyleSheet } from 'react-native';
+import { TouchableWithoutFeedback, Text, Linking, StyleSheet, View } from 'react-native';
 
 // @ts-ignore
 // import Markdown from 'react-native-easy-markdown';
@@ -8,21 +8,9 @@ import { ErrorBoundary } from './ErrorBoundary';
 
 export const CardMarkdown = (props: { text: string }) => (
     <ErrorBoundary>
-        <Markdown
-            style={styles.markdownStyle}
-            // renderLink={(href: string, title: string, children: React.ReactNode) => {
-            //     return (
-            //         <TouchableWithoutFeedback
-            //             key={'linkWrapper_' + href + Date.now()}
-            //             onPress={() => Linking.openURL(href).catch(() => { /* nothing */ })}
-            //         >
-            //             <Text key={'linkWrapper_' + href + Date.now()} style={{textDecorationLine: 'underline'}}>
-            //                 {children}
-            //             </Text>
-            //         </TouchableWithoutFeedback>
-            //     );
-            // }}
-        >{props.text}</Markdown>
+        <View style={styles.markdownStyle}>
+            <Markdown>{props.text}</Markdown>
+        </View>
     </ErrorBoundary>
 );
 
