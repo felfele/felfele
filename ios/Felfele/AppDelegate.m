d2074499d3d4f9dbeb8a3cdc6d6576e7ff9779e1
Mark Vujevits <mark.vujevits@prezi.com>
2019-05-02 14:28:00 +0200

    rename application to felfele in android and ios

--- ios/Felfele/AppDelegate.m
+++ ios/Felfele/AppDelegate.m
@@ -38,7 +38,7 @@
 #endif
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
-                                                      moduleName:@"postmodern"
+                                                      moduleName:@"Felfele"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
