38e61fc73490cf4c5fa3cd9681dccc47bd03e6ac
Attila Gazso <agazso@gmail.com>
2019-05-13 14:04:02 +0200

    WIP, Changed script output

--- scripts/release_new_version.sh
+++ scripts/release_new_version.sh
@@ -11,50 +11,56 @@ function ask {
     done
 }
 
+function output {
+    color="\033[0;32m"
+    reset_color="\033[0m"
+    echo "$color->$1$reset_color"
+}
+
 ask "Write an update to CHANGELOG.md with the changes since last release"
 
-echo "Running checks locally..."
+output "Running checks locally..."
 npm run check
 
-echo "Increasing the version number..."
+output "Increasing the version number..."
 ./scripts/increase_version_number.sh
 
-echo "Determining the version number..."
+output "Determining the version number..."
 version="$(./scripts/cli.sh version)"
-echo "Version: $version"
+output "Version: $version"
 
 release_branch="release-$version"
 git checkout -b $release_branch
-echo "Commit and push changes to the repo"
+output "Commit and push changes to the repo"
 commit_message="Bumped version to $version"
 git commit -am "$commit_message" && git push origin "$release_branch"
 
 ask "Check if the CI is green"
 
-echo "Build the iOS version with XCode for archive..."
-./scripts/build_xcode_archive.sh
+output "Build the iOS version with XCode for archive..."
+#./scripts/build_xcode_archive.sh
 
 ask "Upload the build to the App Store"
 
 ask "In AppstoreConnect provide the crypto information to enable the new build"
 
-echo "Building the android release version signed with debug key..."
-./scripts/build_android_signed_release.sh
+output "Building the android release version signed with debug key..."
+#./scripts/build_android_signed_release.sh
 
-echo "Uploading the android version to https://app.felfele.com..."
-./scripts/upload_android_release.sh "$version"
+output "Uploading the android version to https://app.felfele.com..."
+#./scripts/upload_android_release.sh "$version"
 
 ask "Upload the build to the Play Store"
 
 ask "Download the released versions and do manual QA (both android and iOS)"
 
-echo "Tagging the git release with v$version..."
+output "Tagging the git release with v$version..."
 ./scripts/git_tag_release.sh "$version"
 
-echo "Updating the git release branch with to the master..."
-ask "TODO: ./scripts/git_update_release_branch.sh $release_branch"
+output "Updating the git release branch with to the master..."
+./scripts/git_update_release_branch.sh "$release_branch"
 
 ask "Merge the release branch to master on Github"
 
 ask "Announce the release on Slack in the #releases channel"
-echo "Release of version $version was successful!"
+output "Release of version $version was successful!"
