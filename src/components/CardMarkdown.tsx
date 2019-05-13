e37891dcdc262ece51422124c0c41f0b6b5f41fd
Attila Gazso <agazso@gmail.com>
2019-05-13 15:45:50 +0200

    Removed react-native-easy-markdown

--- src/components/CardMarkdown.tsx
+++ src/components/CardMarkdown.tsx
@@ -1,43 +1,16 @@
 import * as React from 'react';
-import { TouchableWithoutFeedback, Text, Linking, StyleSheet, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
-// @ts-ignore
-import * as EasyMarkdown from 'react-native-easy-markdown';
-import * as MarkdownRenderer from 'react-native-markdown-renderer';
+import Markdown from 'react-native-markdown-renderer';
 import { ErrorBoundary } from './ErrorBoundary';
 
-const CardMarkdownRenderer = (props: { text: string }) => (
+export const CardMarkdown = (props: { text: string }) => (
     <ErrorBoundary>
         <View style={styles.markdownStyle}>
-            <MarkdownRenderer>{props.text}</MarkdownRenderer>
+            <Markdown>{props.text}</Markdown>
         </View>
     </ErrorBoundary>
 );
-
-const CardMarkdownEasy = (props: { text: string }) => {
-    let counter = 0;
-    return (
-        <ErrorBoundary>
-            <EasyMarkdown
-                style={styles.markdownStyle}
-                renderLink={(href: string, title: string, children: React.ReactNode) => {
-                    return (
-                        <TouchableWithoutFeedback
-                            key={'linkWrapper_' + href + counter++}
-                            onPress={() => Linking.openURL(href).catch(() => { /* nothing */ })}
-                        >
-                            <Text key={'linkWrapperText_' + href + counter++} style={{textDecorationLine: 'underline'}}>
-                                {children}
-                            </Text>
-                        </TouchableWithoutFeedback>
-                    );
-                }}
-            >{props.text}</EasyMarkdown>
-        </ErrorBoundary>
-    );
-};
-
-export const CardMarkdown = CardMarkdownEasy;
 
 const styles = StyleSheet.create({
     markdownStyle: {
