d0dee016f17a4795e9039b9f7fbfa564e0b4948d
Attila Gazso <agazso@gmail.com>
2019-06-27 17:08:58 +0200

    Added ContactView

---
+++ src/ui/screens/contact/ContactView.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+
+import { RefreshableFeed } from '../../../components/RefreshableFeed';
+import { Feed } from '../../../models/Feed';
+import { Post } from '../../../models/Post';
+import { NavigationHeader, HeaderDefaultLeftButtonIcon } from '../../../components/NavigationHeader';
+import { ReactNativeModelHelper } from '../../../models/ReactNativeModelHelper';
+import { TypedNavigation } from '../../../helpers/navigation';
+import { MutualContact, Contact } from '../../../models/Contact';
+
+export interface DispatchProps {
+    onConfirmContact: (contact: MutualContact) => void;
+    onRemoveContact: (contact: Contact) => void;
+    onRefreshPosts: (feeds: Feed[]) => void;
+}
+
+export interface UnknownContact {
+    type: 'unknown-contact';
+}
+
+export interface StateProps {
+    navigation: TypedNavigation;
+    onBack: () => void;
+    contact: Contact | UnknownContact;
+    posts: Post[];
+    feed: Feed;
+    gatewayAddress: string;
+}
+
+type Props = StateProps & DispatchProps;
+
+export const ContactView = (props: Props) => {
+    const modelHelper = new ReactNativeModelHelper(props.gatewayAddress);
+    const refreshableFeedProps = {
+        ...props,
+        feeds: [props.feed],
+    };
+    return (
+        <RefreshableFeed modelHelper={modelHelper} {...refreshableFeedProps}>
+            {{
+                navigationHeader: <NavigationHeader
+                    navigation={props.navigation}
+                    leftButton={{
+                        onPress: props.onBack,
+                        label: HeaderDefaultLeftButtonIcon,
+                    }}
+                    title={'Contact'}
+                />,
+            }}
+        </RefreshableFeed>
+    );
+};
---
+++ src/ui/screens/contact/ContactViewContainer.ts
@@ -0,0 +1,57 @@
+import { connect } from 'react-redux';
+import { AppState } from '../../../reducers/AppState';
+import * as Actions from '../../../actions/Actions';
+import { StateProps, DispatchProps, ContactView, UnknownContact } from './ContactView';
+import { TypedNavigation } from '../../../helpers/navigation';
+import { Feed } from '../../../models/Feed';
+import { Contact, MutualContact } from '../../../models/Contact';
+import { Debug } from '../../../Debug';
+
+const findContactByPublicKey = (publicKey: string, contacts: Contact[]): Contact | UnknownContact => {
+    return contacts.find(
+        c => c.type === 'mutual-contact' &&
+        c.identity.publicKey === publicKey
+    )
+    ||
+    {
+        type: 'unknown-contact',
+    };
+};
+
+const mapStateToProps = (state: AppState, ownProps: { navigation: TypedNavigation }): StateProps => {
+    const publicKey = ownProps.navigation.getParam<'ContactView', 'publicKey'>('publicKey');
+    const feed = ownProps.navigation.getParam<'ContactView', 'feed'>('feed');
+    const contact = findContactByPublicKey(publicKey, state.contacts);
+    const posts = contact.type !== 'mutual-contact'
+        ? []
+        : feed.posts
+    ;
+    Debug.log('ContactViewContainer.mapStateToProps', {feed, contact, posts});
+    return {
+        navigation: ownProps.navigation,
+        onBack: () => ownProps.navigation.popToTop(),
+        posts,
+        feed,
+        contact,
+        gatewayAddress: state.settings.swarmGatewayAddress,
+    };
+};
+
+const mapDispatchToProps = (dispatch: any): DispatchProps => {
+    return {
+        onConfirmContact: (contact: MutualContact) => {
+
+        },
+        onRemoveContact: (contact: Contact) => {
+
+        },
+        onRefreshPosts: (feeds: Feed[]) => {
+            dispatch(Actions.AsyncActions.downloadPostsFromFeeds(feeds));
+        },
+    };
+};
+
+export const ContactViewContainer = connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(ContactView);
