4dc596ce99b7a5b476a52419c6385dcc61b621f5
Mark Vujevits <mark.vujevits@prezi.com>
2019-07-08 13:41:41 +0200

    enable sharing in android

---
+++ android/app/src/main/java/org/felfele/mobile/share/ShareActivity.java
@@ -0,0 +1,14 @@
+package org.felfele.mobile.share;
+
+
+// import ReactActivity
+import com.facebook.react.ReactActivity;
+
+
+public class ShareActivity extends ReactActivity {
+    @Override
+    protected String getMainComponentName() {
+        return "Share";
+    }
+
+}
