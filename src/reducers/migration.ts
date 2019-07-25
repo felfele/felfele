a30b710affb52bdda0809d8f9e028e1fb75eddff
Mark Vujevits <mark.vujevits@prezi.com>
2019-07-25 23:15:09 +0200

    fixed error in migration code

--- src/reducers/migration.ts
+++ src/reducers/migration.ts
@@ -4,11 +4,12 @@ import { migrateVersion0ToVersion1 } from './version1';
 import { migrateVersion1ToVersion2 } from './version2';
 import { migrateVersion2ToVersion3 } from './version3';
 import { migrateVersion3ToVersion4 } from './version4';
+import { currentAppStateVersion } from './AppState';
 
 export const migrateAppState: MigrationManifest = {
     0: migrateUnversionedToVersion0,
     1: migrateVersion0ToVersion1,
     2: migrateVersion1ToVersion2,
     3: migrateVersion2ToVersion3,
-    currentAppStateVersion: migrateVersion3ToVersion4,
+    [currentAppStateVersion]: migrateVersion3ToVersion4,
 };
