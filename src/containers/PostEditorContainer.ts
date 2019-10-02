53046675c7d31d3cf24521b51eafebae2ee85464
Mark Vujevits <mark.vujevits@prezi.com>
2019-10-02 17:13:03 +0200

    posting from inside a channel should post directly there, instead of navigating to the share with screen

--- src/containers/PostEditorContainer.ts
+++ src/containers/PostEditorContainer.ts
@@ -5,6 +5,8 @@ import { Post } from '../models/Post';
 import { TypedNavigation } from '../helpers/navigation';
 import { Actions } from '../actions/Actions';
 import { Debug } from '../Debug';
+import { isContactFeed } from '../helpers/feedHelpers';
+import { AsyncActions } from '../actions/asyncActions';
 
 const mapStateToProps = (state: AppState, ownProps: { navigation: TypedNavigation }): StateProps => {
     const goBack = () => {
@@ -21,21 +23,29 @@ const mapStateToProps = (state: AppState, ownProps: { navigation: TypedNavigatio
 };
 
 export const mapDispatchToProps = (dispatch: any, ownProps: { navigation: TypedNavigation }): DispatchProps => {
-   return {
-       onPost: (post: Post) => {
+    return {
+        onPost: (post: Post) => {
             const selectedFeeds = ownProps.navigation.getParam<'Post', 'selectedFeeds'>('selectedFeeds');
-            ownProps.navigation.navigate('ShareWithContainer', {
-                post,
-                selectedFeeds,
-            });
-       },
-       onSaveDraft: (draft: Post) => {
-           dispatch(Actions.addDraft(draft));
-       },
-       onDeleteDraft: () => {
-           dispatch(Actions.removeDraft());
-       },
-   };
+            const feed = selectedFeeds[0];
+            if (selectedFeeds.length === 1 && isContactFeed(feed)) {
+                dispatch(AsyncActions.shareWithContactFeeds(post, [feed]));
+                ownProps.navigation.navigate('ContactView', {
+                    publicKey: feed.contact.identity.publicKey,
+                });
+            } else {
+                ownProps.navigation.navigate('ShareWithContainer', {
+                    post,
+                    selectedFeeds,
+                });
+            }
+        },
+        onSaveDraft: (draft: Post) => {
+            dispatch(Actions.addDraft(draft));
+        },
+        onDeleteDraft: () => {
+            dispatch(Actions.removeDraft());
+        },
+    };
 };
 
 export const PostEditorContainer = connect(
