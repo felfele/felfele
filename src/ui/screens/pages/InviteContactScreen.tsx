d383c018b763738d808486ad25a44e8e3f128007
Attila Gazso <agazso@gmail.com>
2019-12-11 21:11:38 +0100

    Fixed text alignment

--- src/ui/screens/pages/InviteContactScreen.tsx
+++ src/ui/screens/pages/InviteContactScreen.tsx
@@ -118,6 +118,7 @@ const styles = StyleSheet.create({
         padding: 9,
         flexDirection: 'row',
         justifyContent: 'space-between',
+        alignItems: 'center',
     },
     changedYourMindLabel: {
         fontSize: 14,
