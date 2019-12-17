15f6250dc33e4312d34cb8320da11d02250274f0
Attila Gazso <agazso@gmail.com>
2019-12-17 16:53:04 +0100

    Added generateIdentity benchmark

--- src/cli/benchmarkCommands.ts
+++ src/cli/benchmarkCommands.ts
@@ -1,13 +1,14 @@
 import { addCommand } from './cliParser';
 import { output } from './cliHelpers';
 import { deriveSharedKey } from '../helpers/contactHelpers';
+import * as Swarm from '../swarm/Swarm';
+import { stringToUint8Array } from '../helpers/conversion';
+
+const parseNumber = (s: string = '1') => parseInt(s, 10);
 
 export const benchmarkCommandDefinition =
     addCommand('deriveSharedKey [num]', '', async (optionalNum?: string) => {
-        const num = parseInt(optionalNum != null
-            ? optionalNum
-            : '1'
-        , 10);
+        const num = parseNumber(optionalNum);
         const testIdentity = {
             privateKey: '0xebe044bec1031e8e2fa494620dce3e50111a9f5336c358dc08d4d785e4c62ead',
             publicKey: '0x029fbcea33cea864a1b6ec940dd6ba5d7b5d38f4cfe4da7cfc4bc6072bfc750e36',
@@ -21,6 +22,21 @@ export const benchmarkCommandDefinition =
         const startDate = Date.now();
         for (let i = 0; i < num; i++) {
             const sharedSecret = deriveSharedKey(testIdentity, contactIdentity);
+        }
+        const elapsed = Date.now() - startDate;
+        output(`Number of iterations: ${num}, elapsed: ${elapsed} msec, avg: ${elapsed / num} msec`);
+    })
+    .
+    addCommand('generateIdentity [num]', '', async (optionalNum?: string) => {
+        const num = parseNumber(optionalNum);
+        const randomUint8Array = stringToUint8Array(
+            '0xebe044bec1031e8e2fa494620dce3e50111a9f5336c358dc08d4d785e4c62ead'
+        );
+        const promiseUint8Array = Promise.resolve(randomUint8Array);
+        const generateRandom = () => promiseUint8Array;
+        const startDate = Date.now();
+        for (let i = 0; i < num; i++) {
+            await Swarm.generateSecureIdentity(generateRandom);
         }
         const elapsed = Date.now() - startDate;
         output(`Number of iterations: ${num}, elapsed: ${elapsed} msec, avg: ${elapsed / num} msec`);
